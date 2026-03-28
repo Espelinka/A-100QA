bb7b0fc09a14d5a3655f8434b5f601f5d284a5b3
Espelinka <u.mukhlia@gmail.com>
2026-03-28 22:24:09 +0300

    Удален webpack конфиг для совместимости с Next 16

--- next.config.mjs
+++ next.config.mjs
@@ -14,18 +14,6 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (config) => {
-    config.resolve.alias.canvas = false;
-    // Add rule to parse mjs correctly
-    config.module.rules.push({
-      test: /\.m?js$/,
-      type: "javascript/auto",
-      resolve: {
-        fullySpecified: false,
-      },
-    });
-    return config;
-  },
 };
 
 export default nextConfig;
